c17f6e8c6207af745d10f907d4a0dafd6598d122
williamsears <wpeter1@hotmail.com>
2015-11-13 18:53:58 +0000

    Added numSum function and added necessary comments

--- answers.js
+++ answers.js
@@ -9,7 +9,7 @@ characterSelector("DecodeMTL");
 
 // Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
 function lastCharacterSelector(input) {
-    return console.log(input.charAt(input.length-1))
+    return console.log(input.charAt(input.length - 1))
 }
 // Initialize the function "lastCharacterSelector"
 lastCharacterSelector("DecodeMTL")
@@ -21,3 +21,15 @@ function numberedString(inputstring, inputnumber) {
 // Executes the function numberedString
 numberedString("DecodeMTL", 3);
 numberedString("", 1);
+
+//Write a function that takes two numbers and adds them together. Test your function on a few inputs. Write in the comments what happens when you pass something other than a number to your function.
+function numSum(valueone, valuetwo) {
+    var result = valueone + valuetwo;
+  return console.log(result);
+}
+
+//Executes the function NumSum which allows you to add two numbers together.
+numSum(10, 10)
+numSum(10, word)
+
+// When i add a word to my numSum function i get ReferenceError: word is not defined
