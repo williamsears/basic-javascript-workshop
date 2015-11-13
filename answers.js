2d5e3cb9345a50f14e9effef9e27f4cb65cc6be0
williamsears <wpeter1@hotmail.com>
2015-11-13 18:41:40 +0000

    Added the second function; "Last Character Selector"

--- answers.js
+++ answers.js
@@ -2,7 +2,14 @@
 
 //The following function takes the first character from the provided string and displays it in the console log
 function characterSelector(string) {
-    console.log(string.charAt(0));
+    return console.log(string.charAt(0));
 }
 //Executes the following
 characterSelector("DecodeMTL");
+
+// Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
+function lastCharacterSelector(input) {
+    return console.log(input.charAt(input.length-1))
+}
+// Initialize the function "lastCharacterSelector"
+lastCharacterSelector("DecodeMTL")
