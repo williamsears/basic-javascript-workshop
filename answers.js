d3fe1ea797d9710e3d72af5e2c3e739787e2e3f0
williamsears <wpeter1@hotmail.com>
2015-11-13 18:35:05 +0000

    added function to select the first character of the string with corresponding commments

--- answers.js
+++ answers.js
@@ -1,1 +1,8 @@
 //This is a one line comment in a javascript file.
+
+//The following function takes the first character from the provided string and displays it in the console log
+function characterSelector(string) {
+    console.log(string.charAt(0));
+}
+//Executes the following
+characterSelector("DecodeMTL");
